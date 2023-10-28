cb1d4e756019b764e034772749c8e18be46016e3
KentosFc <132023095+KentosFc@users.noreply.github.com>
2023-10-28 23:27:11 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//OPSI MENU
+let rutinitas = document.getElementById("rutinitas")
+let berzanji = document.getElementById("berzanji")
+let about = document.getElementById("about")
+
+
+about.addEventListener("click", function() {
+    window.location.href = "about.html"
+})
+
+rutinitas.addEventListener("click", function() {
+    window.location.href = "rutinitas.html"
+})
+
+berzanji.addEventListener("click", function() {
+    window.location.href = "berzanji.html"
+})
